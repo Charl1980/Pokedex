f41c001501fad18d97ae908a3bbf13f391232636
Charl <94119475+Charl1980@users.noreply.github.com>
2023-08-30 13:55:01 +0200

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let myName = 'Charl';
+document.write(myName);
+myName = 'Carlos';
+document.write('Hello! My name is ' + myName);
+
+/* let simpleAddition = (2 + 2 + 6 - 7) * 9;
+document.write(simpleAddition); 
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize); */
